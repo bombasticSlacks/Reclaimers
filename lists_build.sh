fc00eb3211a06831f1ee468723ded679a2cb66f8
bombasticSlacks <dillon.matchett@gmail.com>
2024-03-07 10:10:53 -0400

    Update lists_build.sh

--- lists_build.sh
+++ lists_build.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
-# builds all lists
+# Core Game Lists
+python3 ./Game/Core/Scripts/lists.py Game/Core/Weapon-Traits.md Weapon-Traits Game/Core/Blocks
+python3 ./Game/Core/Scripts/lists.py Game/Core/Armour-Traits.md Armour-Traits Game/Core/Blocks
+python3 ./Game/Core/Scripts/lists.py Game/Core/Gear.md Gear-Trait Game/Blocks
+python3 ./Game/Core/Scripts/lists.py Game/Core/Gear.md Gear-Trait Game/Core/Blocks
+# Reclaimers Lists
 python3 ./Game/Core/Scripts/lists.py Game/Specialization-List.md Specialization-List Game/Blocks
 python3 ./Game/Core/Scripts/lists.py Game/Armour-List.md Armour-List Game/Blocks
 python3 ./Game/Core/Scripts/lists.py Game/Weapons-List.md Weapons-List Game/Blocks
@@ -9,10 +14,9 @@ python3 ./Game/Core/Scripts/lists.py Game/Expert-List.md Expert-List Game/Blocks
 python3 ./Game/Core/Scripts/lists.py Game/Augment-List.md Augment-List Game/Blocks
 python3 ./Game/Core/Scripts/lists.py Game/Training-List.md Training-List Game/Blocks
 python3 ./Game/Core/Scripts/lists.py Game/Modification-List.md Modification-List Game/Blocks
-python3 ./Game/Core/Scripts/lists.py Game/Core/Weapon-Traits.md Weapon-Traits Game/Core/Blocks
-python3 ./Game/Core/Scripts/lists.py Game/Core/Armour-Traits.md Armour-Traits Game/Core/Blocks
-python3 ./Game/Core/Scripts/lists.py Game/Core/Gear.md Gear-Trait Game/Blocks
-python3 ./Game/Core/Scripts/lists.py Game/Core/Gear.md Gear-Trait Game/Core/Blocks
+# Creatures
+python3 ./Game/Core/Scripts/lists.py Game/Core/Black-Mass-Creatures.md Black-Mass-Creatures Game/Blocks
+
 
 
 
