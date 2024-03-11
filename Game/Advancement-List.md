7fcb54795ce66db84d6ab1c25e45415531c42f29
bombasticSlacks <dillon.matchett@gmail.com>
2024-03-11 09:02:27 -0300

    Testing Filtering All

---
+++ Game/Advancement-List.md
@@ -0,0 +1,21 @@
+---
+layout: default
+title: Advancement List
+has_children: false
+parent: Progress
+grand_parent: Telling The Story
+nav_order: 1
+---
+# Advancement List
+{: .no_toc }
+
+<details open markdown="block">
+  <summary>
+    Table of contents
+  </summary>
+  {: .text-delta }
+- TOC
+{:toc}
+</details>
+
+<fieldset class="filter"><legend>Filter Advancements:</legend></fieldset>
--- lists_build.sh
+++ lists_build.sh
@@ -5,15 +5,10 @@ python3 ./Game/Core/Scripts/lists.py Game/Core/Armour-Traits.md Armour-Traits Ga
 python3 ./Game/Core/Scripts/lists.py Game/Core/Gear.md Gear-Trait Game/Blocks
 python3 ./Game/Core/Scripts/lists.py Game/Core/Gear.md Gear-Trait Game/Core/Blocks
 # Reclaimers Lists
-python3 ./Game/Core/Scripts/lists.py Game/Specialization-List.md Specialization-List Game/Blocks
 python3 ./Game/Core/Scripts/lists.py Game/Armour-List.md Armour-List Game/Blocks
 python3 ./Game/Core/Scripts/lists.py Game/Weapons-List.md Weapons-List Game/Blocks
 python3 ./Game/Core/Scripts/lists.py Game/Gear-List.md Gear-List Game/Blocks
-python3 ./Game/Core/Scripts/lists.py Game/Basic-List.md Basic-List Game/Blocks
-python3 ./Game/Core/Scripts/lists.py Game/Expert-List.md Expert-List Game/Blocks
-python3 ./Game/Core/Scripts/lists.py Game/Augment-List.md Augment-List Game/Blocks
-python3 ./Game/Core/Scripts/lists.py Game/Training-List.md Training-List Game/Blocks
-python3 ./Game/Core/Scripts/lists.py Game/Modification-List.md Modification-List Game/Blocks
+python3 ./Game/Core/Scripts/lists.py Game/Advancement-List.md Basic-List,Expert-List Game/Blocks
 # Creatures
 python3 ./Game/Core/Scripts/lists.py Game/Black-Mass-Creatures.md Black-Mass-Creatures Game/Creatures
 
